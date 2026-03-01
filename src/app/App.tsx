66eb7bb4cf963fbf8af6bfe305dc25eb405f1d65
Lynx141 <omarmabbas.p@gmail.com>
2026-03-01 02:48:36 +0200

    Added Brevo form

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
+import BrevoForm from "./BrevoForm";
 
+<BrevoForm />
 function App() {
   return <RouterProvider router={router} />;
 }
